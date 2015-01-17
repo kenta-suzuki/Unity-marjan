f838fa37b4b3eb017e7d80e56368ada1e904790f
kenta-suzuki <kenta.suzuki@befool.co.jp>
2015-01-17 11:09:00 +0900

    テスト
    プロジェクト追加

---
+++ Photon/Assets/PhotonTestScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonTestScript : MonoBehaviour {
+
+	string unitychan = "unitychan";
+	private PhotonView myPhotonView;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log("JoinRandom");
+		PhotonNetwork.JoinRandomRoom();
+	}
+	
+	void OnPhotonRandomJoinFailed()
+	{
+		PhotonNetwork.CreateRoom(null);
+	}
+	
+	void OnJoinedRoom()
+	{
+		GameObject monster = (GameObject) PhotonNetwork.Instantiate(unitychan, Vector3.zero, Quaternion.identity, 0);
+		//monster.GetComponent<myThirdPersonController>().isControllable = true;
+		myPhotonView = monster.GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
